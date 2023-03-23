60c926dd01ef9725c328049065e93c94ecd4080e
kyungho-dev <kyunghoma2@naver.com>
2023-03-23 21:00:38 +0900

    #bigO : 시작하기! 1에서 n까지의 합

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // test
 
 const init = () => {
-  console.log("HELLO!")
+  console.log("IT's init console!");
+  console.log("-------------------------------------");
+  console.log("-------------------------------------");
+
+  bigONotation();
+
+}
+
+const bigONotation = () => {
+  function addUpTo(n) {
+    let total = 0;
+    for (let i = 1 ; i  <= n ; i++) {
+      total += i;
+    }
+    return total;
+  }
+
+  function addUpTo2(n) {
+    return n * (n + 1) / 2;
+  }
+  console.log(addUpTo(100));
+  console.log(addUpTo2(100));
 }
 
 
