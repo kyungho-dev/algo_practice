f0aa2cdb853e0764af21e073d30cfc81719d6925
kyungho-dev <kyunghoma2@naver.com>
2023-03-26 18:47:44 +0900

    #알고리즘 강의 : 문자열 입력받아 해당 알파벳의 갯수 출력하기(charCount() 함수 생성)

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const init = () => {
   console.log("-------------------------------------");
   console.log("-------------------------------------");
 
-  bigONotation();
+  // bigONotation();
+
+  // 문자열 갯수
+  console.log(charCount("hello"));
 
 }
 
@@ -23,6 +26,34 @@ const bigONotation = () => {
   }
   console.log(addUpTo(100));
   console.log(addUpTo2(100));
+}
+
+function charCount(str) {
+  // 마지막에 반환할 객체 만들기
+  // 문자열 순환 (문자열 내 각 char)
+    // 만약 순환하며 만나는 char가 'number/문자열이고', '객체에 key로 존재하면' 해당 키의 값에 + 1
+    // 만약 순환하며 만나는 char가 'number/문자열이고', '객체에 없다면', 해당 char를 키로 추가하고 value를 1로 셋
+    // 만약 순환하며 만나는 char가 다른 어떤 것(공백, 마침표 etc.)라면 아무것도 하지 않음
+  // 결과값 return
+
+  // 마지막에 반환할 객체 만들기
+  const result = {};
+  // 문자열 순환 (문자열 내 각 char)
+  for(let i = 0 ; i < str.length ; i++) {
+    let char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {  // 알파벳 혹은 숫자
+      if (result[char] > 0) { // 0보다 크면 이미 객체에 조재한다는 것
+        // 만약 순환하며 만나는 char가 'number/문자열이고', '객체에 key로 존재하면' 해당 키의 값에 + 1
+        result[char]++;
+      } else {  // 아직 ㅐㄱ체에 없다면
+        // 만약 순환하며 만나는 char가 'number/문자열이고', '객체에 없다면', 해당 char를 키로 추가하고 value를 1로 셋
+        result[char] = 1;
+      }
+    }
+  }
+  // 만약 순환하며 만나는 char가 다른 어떤 것(공백, 마침표 etc.)라면 아무것도 하지 않음
+  // 결과값 return
+  return result;
 }
 
 
