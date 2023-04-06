714503b3b49a6eeac603a2723c61fb4dd4396ff2
kyungho-dev <kyunghoma2@naver.com>
2023-04-06 22:37:18 +0900

    #algo : 배열과 함수를 인자로 받아 함수를 통과하는 값이 있는 경우 true를 반환하는 함수 작성

--- index.js
+++ index.js
@@ -706,4 +706,25 @@ function isPalindrome(str){
 }
 
 
+// 첫번째 인자 : 배열
+// 두번째 인자 : 함수
+// 첫번째 인자의 값이 두번째 인자인 함수에 통과(TRUE)하는 값이
+// 있는 경우 true, 아니면 false return 하는 함수
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(arr, func){
+
+  if (arr.length === 0) return false;
+  if (func(arr[0])) return true;
+  return someRecursive(arr.slice(1), func);
+
+
+}
+
 init();
