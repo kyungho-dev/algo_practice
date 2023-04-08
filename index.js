17a7ba17ac976b2950b8408f07f99f73a99fa428
kyungho-dev <kyunghoma2@naver.com>
2023-04-08 23:38:28 +0900

    #algo : 배열을 받아 대문자로 만드는 함수

--- index.js
+++ index.js
@@ -739,4 +739,16 @@ function flatten(oldArr){
 }
 
 
+// 대문자로 만드는 함수
+function capitalizeWords (array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+
+}
+
+
 init();
