8b59400f09a96a4c69c71128f9d656932fff4ab0
kyungho-dev <kyunghoma2@naver.com>
2023-04-15 19:42:03 +0900

    #algo : 선택 정렬 예제

---
+++ search_algorithm/selectionSort.js
@@ -0,0 +1,24 @@
+// 선택 정렬
+
+// pseudocode
+// 1. 최솟값을 저장할 변수 만들기, 정렬의 첫번째 값이 가장 작은 값
+// 2. 진행하며 다음 항목과 비교, 다음 항목이 작을 경우 해당 다음 항목을 "가장 작은 변수" 값으로 갱신
+// 3. 처음 시작한 값, 인덱스가 최솟값이 아니라면 두 값을 바꾼다.
+function selectionSort(arr) {
+  let minIdx = 0;
+
+  for (let i = 0 ; i < arr.length ; i++) {
+    minIdx = i;
+    for (let j = i + 1 ; j < arr.length ; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j
+      }
+    }
+    if (minIdx !== i) {
+      let temp = arr[minIdx];
+      arr[minIdx] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
