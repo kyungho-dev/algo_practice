5da5d9c007fd276e90a08d4a0369c9e0153eb4b1
kyungho-dev <kyunghoma2@naver.com>
2023-04-16 21:27:26 +0900

    #algo : 선택 정렬 해설 코드, ES 2015 적용한 버전

--- search_algorithm/selectionSort.js
+++ search_algorithm/selectionSort.js
@@ -22,3 +22,40 @@ function selectionSort(arr) {
   }
   return arr;
 }
+
+function selectionSortSol(arr) {
+  for (let i = 0 ; i < arr.length ; i++) {
+    let lowest = i;
+    for (let j = i + 1 ; j < arr.length ; j++) {
+      if (arr[j] > arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+// ES2015 적용한 방법
+function selectionSortSol2(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  }
+
+  for (let i = 0 ; i < arr.length ; i++) {
+    let lowest = i;
+    for (let j = i + 1 ; j < arr.length ; j++) {
+      if (arr[j] > arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      swap(arr, i , lowest);
+    }
+  }
+  return arr;
+}
