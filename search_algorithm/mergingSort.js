37553ad6d8d892829bbf46f4a264c0059f4dec4b
kyungho-dev <kyunghoma2@naver.com>
2023-04-18 22:00:05 +0900

    #algo : 병합정렬 예제 강사코드

--- search_algorithm/mergingSort.js
+++ search_algorithm/mergingSort.js
@@ -14,3 +14,28 @@ function mergingSort(arr, arr2) {
   }
   return answer;
 }
+
+// 강사코드
+function mergingSortSol(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
