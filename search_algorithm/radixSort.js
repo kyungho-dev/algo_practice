8d1d97a1f2e3147b67bdda8a157819d6543217a0
kyungho-dev <kyunghoma2@naver.com>
2023-04-24 22:51:54 +0900

    #algo : 기수정렬 helper함수 작성 (숫자 몇자리인지 확인하는 함수)

--- search_algorithm/radixSort.js
+++ search_algorithm/radixSort.js
@@ -5,4 +5,10 @@ function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
-// Todo :기수 정렬 만들기
+// Radix sort helper 함수
+// 숫자가 몇자리수인지 파악 : 1의 자리 10의 자리...
+function digitCount(num) {
+  // log10 0 은 -Infinity 이므로 예외처리
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
