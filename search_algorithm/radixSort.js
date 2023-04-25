aaf39675a2b38b139845c6ffb6acfc3d9844b1db
kyungho-dev <kyunghoma2@naver.com>
2023-04-25 20:08:18 +0900

    #algo : 기수정렬 구현

--- search_algorithm/radixSort.js
+++ search_algorithm/radixSort.js
@@ -21,3 +21,34 @@ function mostDigits(nums) {
   }
   return maxDigits;
 }
+
+// 기수 정렬 Pseudocode
+// 1. 수 배열을 인자로 받는 함수를 정의
+// 2. 가장 큰 수가 몇 자리 수인지 알아내기
+// 3. 반복문 시작, 0부터 가장 큰 자릿수까지
+// 4. 진행할 때마다, 각 자릿수에 버킷을 만든다. (여기서 버킷은 빈 배열) (하위 배열이 10개 있는 배열이겠지..! 0 ~ 9)
+// 5. 반복문을 수행하면서 각각의 수를 올바른 버킷에 넣는다.
+//   -> kth digit (k 자릿수)란 루프 k가 무엇이든 0에서 시작하며 각 수를 올바른 위치, 올바른 버킷 슬롯에
+//   0자리를 사용하여 분류하고, 다음으로는 첫째 자리, 둘째 자리를 이용한다는 것이다.
+// 6. 기존 배열을 버킷의 값으로 교체하기
+
+// 기수정렬 구현
+function radixSort(arr) {
+
+  const maxDigit = mostDigits(arr);
+  let tempArr = [];
+  let sortedArr = []
+  for (let i = 0 ; i < maxDigit ; i++) {
+    sortedArr = [];
+    for (let j = 0 ; j < 10 ; j++) {
+      tempArr[j] = [];
+      for (let k = 0 ; k < arr.length ; k++) {
+        if (getDigit(arr[k], i) === j) tempArr[j].push(arr[k]);
+      }
+    }
+    for (let j = 0 ; j < 10 ; j++) {
+      sortedArr.concat(tempArr[i]);
+    }
+  }
+  return sortedArr;
+}
