9b649cc45192b1eb021ca1b7be6a101e53882a54
kyungho-dev <kyunghoma2@naver.com>
2023-04-27 21:47:12 +0900

    #자료구조 : instance 속성 및 메서드 추가하기

--- data_structure/index.js
+++ data_structure/index.js
@@ -1,7 +1,38 @@
-class Student {
+class StudentOld {
   constructor(firstName, lastName, year) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.grade = year;
   }
 }
+
+class Student {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.tardies = 0;
+    this.scores = [];
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return "YOU ARE EXPELLED!!!!!";
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    let sum = this.scores.reduce(function(a, b) {return a+b;})
+    return sum / this.scores.length;
+  }
+}
+
+let firstStudent = new Student("Colt", "Steele");
+firstStudent.fullName(); // "Colt Steele"
+// firstStudent가 Student라는 인스턴스의 함수를 사용한 것.
