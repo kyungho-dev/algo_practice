a6f791eea47f5f8104787c12bc80a9466065b11d
kyungho-dev <kyunghoma2@naver.com>
2023-05-04 21:19:24 +0900

    #자료구조 : Singly Linked List의 unshift() 메서드 강사 코드

--- data_structure/singlyLinkedList.js
+++ data_structure/singlyLinkedList.js
@@ -152,6 +152,20 @@ class SinglyLinkedList {
     return this;
   }
 
+  // 강사 코드
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
 }
 
 
